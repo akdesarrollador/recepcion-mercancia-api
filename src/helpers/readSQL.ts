06c5952b72d7952435ff55a98d33aec8e70956f5
akdesarrollador <akdesarrollo1@gmail.com>
2025-06-12 11:40:07 -0400

    feat: optimizing readSql file

--- src/helpers/readSQL.ts
+++ src/helpers/readSQL.ts
@@ -1,8 +1,16 @@
-import fs from 'node:fs'
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const readSQL = (path: string): string => {
-    const result = fs.readFileSync(`./src/queries/` + path + '.sql');
-    return result.toString();
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export default readSQL
+const QUERIES_DIR = path.resolve(__dirname, "../queries");
+
+const readSQL = (filename: string): string => {
+  const filePath = path.join(QUERIES_DIR, `${filename}.sql`);
+  const result = fs.readFileSync(filePath, "utf-8");
+  return result;
+};
+
+export default readSQL;
